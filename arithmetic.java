2e632c7b7d473f602267aff2a947343bf865fe9a
khushi <khusbumandal2060@gmail.com>
2024-01-30 00:24:57 +0545

    codes

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+public class arithmetic {
+    public static void main(String[]args) {
+        int a = 30;
+        int b = 15;
+         int sum = a + b;
+        System.out.println(sum);
+       int diff = a - b;
+    System.out.println(diff); 
+    int mul = a*b;
+    System.out.println(mul); 
+ }
+}
---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum;
+
+    import java.Util.*;
+
+    public class Main {
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
