ba8b6bc9369b28ef553294fbcc9f371e97b70fdc
khushi <khusbumandal2060@gmail.com>
2024-01-31 00:23:47 +0545

    difference between while & do-while loop

--- Loops5.java
+++ Loops5.java
@@ -1,9 +1,10 @@
 public class Loops5 {
     public static void main(String[] args) {
         int i = 12;
-        /* while (i < 11) {
+        while (i < 11) {
             System.out.println("Khushi"); 
-            i++; */
+            i++; 
+        }
 
             do {
                 System.out.println("Khushi");
