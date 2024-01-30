b9ece588efe57ab97ca140ac15cc7885b8a5c0b6
khushi <khusbumandal2060@gmail.com>
2024-01-31 00:18:14 +0545

    do-while loop

---
+++ Loops5.java
@@ -0,0 +1,14 @@
+public class Loops5 {
+    public static void main(String[] args) {
+        int i = 12;
+        /* while (i < 11) {
+            System.out.println("Khushi"); 
+            i++; */
+
+            do {
+                System.out.println("Khushi");
+                i++;
+            } while(i < 11);
+        }
+    }
+
