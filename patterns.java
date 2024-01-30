300333972af6b7a75259882bb974d53376c07f9c
khushi <khusbumandal2060@gmail.com>
2024-01-31 01:12:39 +0545

    Print the table of a number using for loop

--- patterns.java
+++ patterns.java
@@ -1,12 +1,9 @@
 import java.util.*;
-public class Loops7 {
+public class patterns {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = 1; i<11; i++) {
-            System.out.println(i*n);
-            sc.close();
-        }
+        for(int )
     }
 }
