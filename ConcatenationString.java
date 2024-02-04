f1600a626bad5b2230c9fedf59165c84c238f73d
khushi <khusbumandal2060@gmail.com>
2024-02-05 01:34:52 +0545

    Concatenation function is used to add two String

---
+++ ConcatenationString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ConcatenationString {
+    public static void main(String[] args) {
+        
+        //concatenation function
+        String firstName = "Nikhil";
+        String lastName = "Singh";
+        String fullname = firstName +" " +lastName;
+        System.out.println(fullname);
+    }
+}
