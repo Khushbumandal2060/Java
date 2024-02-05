c139012e81fa91e93e79184858941e47d39d211a
khushi <khusbumandal2060@gmail.com>
2024-02-06 02:23:25 +0545

    COmpare 2 Strings

--- String2.java
+++ String2.java
@@ -8,18 +8,21 @@ public class String2 {
         //2 s1 == s2 : 0
         //3 s1 < s2 : -ve value
 
-      // if(name1.compareTo(name2) == 0) {
-      //   System.out.println("String are equal");
-      //  } else {
-      //    System.out.println("Strings are not equal");
-      // }
+       if(name1.compareTo(name2) == 0) {
+         System.out.println("String are equal");
+        } else {
+          System.out.println("Strings are not equal");
+       }
        
-     //  if(name1 == name2) {
-     //    System.out.println("String are equal");
-     // } else {
-     //   System.out.println("Strings are not equal");
-     // }
+       //DO NOT USE == to check for string equality
+       //gives correct answer here
+       if(name1 == name2) {
+         System.out.println("String are equal");
+      } else {
+        System.out.println("Strings are not equal");
+      }
 
+     //gives incorrect answer here
      if(new String("Tony") == new String("Tony")) {
           System.out.println("strings are equal");
      } else {
