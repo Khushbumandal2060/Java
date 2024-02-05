fa42b5ecdee0cb02a46a163b622df25163dac077
khushi <khusbumandal2060@gmail.com>
2024-02-06 02:17:20 +0545

    Substring of a String is a subpart of it

---
+++ SubString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class SubString {
+    public static void main(String[] args) {
+        String sentence = "KhusaStark";
+        String name = sentence.substring(0,5);
+        System.out.println(name);
+    }
+}
