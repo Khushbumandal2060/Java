616c8b32a8949734cff9523345c6c534968a200e
khushi <khusbumandal2060@gmail.com>
2024-02-07 00:08:26 +0545

    Color of the pen

---
+++ Oops.java
@@ -0,0 +1,27 @@
+class pen {
+    String color;
+    String type;
+
+    public void write() {
+        System.out.println("writing something"); 
+    }
+
+    public void printColor() {
+        System.out.println(this.color);
+    }
+}
+
+public class Oops {
+    public static void main(String[] args) {
+        pen pen1 = new pen();
+        pen1.color = "blue";
+        pen1.type ="gel";
+
+        pen pen2 = new pen();
+        pen2.color = "red";
+        pen2.type = "ballpoint";
+
+        pen1.printColor();
+        pen2.printColor();
+    }
+}
