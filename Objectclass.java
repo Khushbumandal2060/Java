2cf902ce27df4b3a88aad96bf2ac6052fc9eaf3e
khushi <khusbumandal2060@gmail.com>
2024-02-07 01:36:05 +0545

    Arithmetic operations

--- Objectclass.java
+++ Objectclass.java
@@ -9,7 +9,7 @@ class Student {
 }
 
 
-public class Objectandclass {
+public class Objectclass {
     public static void main(String[] args) {
         Student s1 = new Student();
         s1.name = "khushi";
